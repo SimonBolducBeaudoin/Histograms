22d08cc3e7f6c71513a4d77a077a2b06bffbaed0
Simon Bolduc Beaudoin <Simon.Bolduc.Beaudoin@usherbrooke.ca>
2024-10-30 15:36:32 -0400

    (Bug) std_2Dmoments_numba not working

--- histograms_helper.py
+++ histograms_helper.py
@@ -16,26 +16,38 @@ Todos :
 Bugs :
 """
 
-import numba as _nb
-@_nb.guvectorize([(_nb.uint32[:,:],_nb.float64[:],_nb.int32[:],_nb.float64,_nb.float64[:,:])], '(n,n),(n),(m),()->(m,m)')
-def std_2Dmoments_numba(histogram,x,order,Cx,res=None):
+def compute_moments2D(Hs,x,order = 8,Cx=None,Cy=None,implementation='numba'):
+    Cx = 1. if Cx is None else Cx
+    Cy = 1. if Cy is None else Cy
+    if implementation == 'numba' :
+        if isinstance(order, int) :
+            order = _np.r_[:order+1]
+        if order.shape[-1] < 3 :
+            raise Exception("Order must be > 2")
+        return std_2Dmoments_numba(Hs,x,order,Cx,Cy)
+    else :
+        raise Exception("No implementation chosen.")
+
+@_nb.guvectorize([(_nb.uint32[:,:],_nb.float64[:],_nb.int32[:],_nb.float64,_nb.float64,_nb.float64[:,:])], '(n,n),(n),(m),(),()->(m,m)', target="parallel")
+def std_2Dmoments_numba(histogram,x,order,Cx,Cy,res=None):
     no_clip=True
     first_bin = 1 if no_clip else 0
     last_bin  = len(x) - 1 if no_clip else len(x)
     x_slice   = slice(first_bin,last_bin)
     y_slice   = x_slice
-    bins = x*Cx
+    bins_x    = x*Cx
+    bins_y    = x*Cy
     
-    hist=histogram[y_slice,x_slice] # no_copy cuz python
+    hist      = histogram[y_slice,x_slice] # no_copy cuz python
 
     moments = _np.zeros( (len(order),len(order)), dtype=_np.float64)
 
     moments[0,0] = hist.sum()
     
-    moments[0,1] = (hist * bins[None,x_slice]).sum()/ moments[0,0]
-    moments[1,0] = (hist * bins[y_slice,None]).sum()/ moments[0,0]
-    binx = (bins-moments[0,1])[None,x_slice] # centered bins
-    biny = (bins-moments[0,1])[y_slice,None] # centered bins
+    moments[0,1] = (hist * bins_x[None,x_slice]).sum()/ moments[0,0]
+    moments[1,0] = (hist * bins_y[y_slice,None]).sum()/ moments[0,0]
+    binx = (bins_x-moments[0,1])[None,x_slice] # centered bins
+    biny = (bins_y-moments[1,0])[y_slice,None] # centered bins
     moments[1,1] = (hist * biny * binx).sum()/ moments[0,0]
     
     moments[0,2] = (hist           * binx**2).sum()/ moments[0,0]
@@ -44,20 +56,54 @@ def std_2Dmoments_numba(histogram,x,order,Cx,res=None):
     moments[2,1] = (hist * biny**2 * binx   ).sum()/ moments[0,0]
     moments[2,2] = (hist * biny**2 * binx**2).sum()/ moments[0,0]
     
-    x_bar = moments[0,1]
-    y_bar = moments[1,0]
-    for j,oy in enumerate(order[3:]):
-        for i,ox in enumerate(order[3:]):
-            moments[j+3,i+3] = (hist* biny**oy * binx**ox).sum()  # <(x - <x>)^o>
-        
-    # normalisation
-    moments[3:,3:] /= moments[0,0]
+    for oy in order[3:]:
+        for ox in order[:3]:
+            moments[oy,ox] = (hist* biny**oy * binx**ox).sum() / (moments[0,0] * moments[2,0]**(oy/2.0) * moments[0,2]**(ox/2.0) )
     
-    stdize = _np.zeros( (len(order),len(order)), dtype=_np.float64)
-    stdize = moments[2,0] ** (order[:,None] / 2.0) * moments[0,2] ** (order[None,:] / 2.0)
-    # Standardisation
-    moments[3:,3:] /= stdize[3:,3:]
+    for oy in order[:]:
+        for ox in order[3:]:
+            moments[oy,ox] = (hist* biny**oy * binx**ox).sum() / (moments[0,0] * moments[2,0]**(oy/2.0) * moments[0,2]**(ox/2.0) )
+    
     res[:] = moments
+    
+@_nb.guvectorize([(_nb.uint64[:,:],_nb.float64[:],_nb.int32[:],_nb.float64,_nb.float64,_nb.float64[:,:])], '(n,n),(n),(m),(),()->(m,m)', target="parallel")
+def std_2Dmoments_numba(histogram,x,order,Cx,Cy,res=None):
+    no_clip=True
+    first_bin = 1 if no_clip else 0
+    last_bin  = len(x) - 1 if no_clip else len(x)
+    x_slice   = slice(first_bin,last_bin)
+    y_slice   = x_slice
+    bins_x    = x*Cx
+    bins_y    = x*Cy
+    
+    hist      = histogram[y_slice,x_slice] # no_copy cuz python
+
+    moments = _np.zeros( (len(order),len(order)), dtype=_np.float64)
+
+    moments[0,0] = hist.sum()
+    
+    moments[0,1] = (hist * bins_x[None,x_slice]).sum()/ moments[0,0]
+    moments[1,0] = (hist * bins_y[y_slice,None]).sum()/ moments[0,0]
+    binx = (bins_x-moments[0,1])[None,x_slice] # centered bins
+    biny = (bins_y-moments[1,0])[y_slice,None] # centered bins
+    moments[1,1] = (hist * biny * binx).sum()/ moments[0,0]
+    
+    moments[0,2] = (hist           * binx**2).sum()/ moments[0,0]
+    moments[2,0] = (hist * biny**2          ).sum()/ moments[0,0]
+    moments[1,2] = (hist * biny    * binx**2).sum()/ moments[0,0]
+    moments[2,1] = (hist * biny**2 * binx   ).sum()/ moments[0,0]
+    moments[2,2] = (hist * biny**2 * binx**2).sum()/ moments[0,0]
+    
+    for oy in order[3:]:
+        for ox in order[:3]:
+            moments[oy,ox] = (hist* biny**oy * binx**ox).sum() / (moments[0,0] * moments[2,0]**(oy/2.0) * moments[0,2]**(ox/2.0) )
+    
+    for oy in order[:]:
+        for ox in order[3:]:
+            moments[oy,ox] = (hist* biny**oy * binx**ox).sum() / (moments[0,0] * moments[2,0]**(oy/2.0) * moments[0,2]**(ox/2.0) )
+    
+    res[:] = moments
+
 
 @_nb.guvectorize([(_nb.uint64[:],_nb.float64[:],_nb.int32[:],_nb.float64,_nb.float64[:])], '(n),(n),(m),()->(m)')
 def std_moments_numba(histogram,x,order,Cx,res=None):
